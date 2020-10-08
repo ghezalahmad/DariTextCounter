c904446ae54af47151e103c6ad0de9a73c3648be
ghezal ahmad zia <Ghezal Ahmad>
2020-10-09 01:10:14 +0200

    Dari Word Count Web App

--- wordcount-project/wordcount/urls.py
+++ wordcount-project/wordcount/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('', views.homepage, name='home'),
+    path('count/', views.count, name='count'),
+    path('about/', views.about, name='about'),
 ]
